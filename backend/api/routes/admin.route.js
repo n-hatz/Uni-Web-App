c6e911b8165f7020d218137411cdb72138525c56
n-hatz <hatzisniktas@gmail.com>
2021-10-06 18:49:04 +0300

    added routes

--- backend/api/routes/admin.route.js
+++ backend/api/routes/admin.route.js
@@ -5,9 +5,11 @@ import auth from '../middleware/auth.js';
 const router = express.Router();
 
 router.post('/signin',controller.sign_in);
-router.get('/student/:id',auth,controller.get_student);
+router.get('/search',auth,controller.search_student);
+router.get('/students/',auth,controller.get_students);
+router.get('/students/:id',auth,controller.get_student_by_id);
 router.post('/grades/:id',auth,controller.add_grade);
 router.patch('/grades/:id',auth,controller.update_grade);
-router.delete('/grade/:id',auth,controller.delete_grade);
+router.delete('/students/:id/grades/:grade_id',auth,controller.delete_grade);
 
 export default router;
