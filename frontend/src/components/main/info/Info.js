09fd13e49218d50e411c8df6e623f81e21599f06
n-hatz <hatzisnikitas@gmail.com>
2021-10-08 17:04:49 +0300

    modified list

--- frontend/src/components/main/info/Info.js
+++ frontend/src/components/main/info/Info.js
@@ -6,15 +6,16 @@ const Info = () => {
         <Container maxWidth="xl">
             <Paper style={{ padding: '20px', borderRadius: '15px' }} raised elevation={6}>
                 <Typography variant="h6">
-                    Uni-Web-App project.
+                    Uni-Web-App project. &nbsp;
+                    <Link href="https://github.com/n-hatz/Uni-Web-App">Github</Link>
                 </Typography>
-                <Link href="https://github.com/n-hatz/Uni-Web-App">Github</Link>
+                <Typography variant="body2">Layout,styling inspired from tutorial.</Typography>
+                
                 <Typography vairant="body1">Features:</Typography>
                 <ul>
                     <li>CRUD Forum</li>
                     <li>Authentication</li>
                     <li>Admin control panel</li>
-                    <li>Non-existent/minimal styling</li>
                 </ul>
             </Paper>
         </Container>
