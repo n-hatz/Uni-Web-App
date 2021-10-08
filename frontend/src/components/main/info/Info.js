a448bdaa9b2b1277c628a1d939f6f13988add525
n-hatz <hatzisnikitas@gmail.com>
2021-10-08 16:39:46 +0300

    added features list

--- frontend/src/components/main/info/Info.js
+++ frontend/src/components/main/info/Info.js
@@ -6,13 +6,17 @@ const Info = () => {
         <Container maxWidth="xl">
             <Paper style={{ padding: '20px', borderRadius: '15px' }} raised elevation={6}>
                 <Typography variant="h6">
-                    Welcome to Uni-Web-App project.
+                    Uni-Web-App project.
                 </Typography>
                 <Link href="https://github.com/n-hatz/Uni-Web-App">Github</Link>
+                <Typography vairant="body1">Features:</Typography>
+                <ul>
+                    <li>CRUD Forum</li>
+                    <li>Authentication</li>
+                    <li>Admin control panel</li>
+                    <li>Non-existent/minimal styling</li>
+                </ul>
             </Paper>
-            <Typography>
-                Fix css mate
-            </Typography>
         </Container>
      );
 }
